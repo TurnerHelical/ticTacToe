8376c2816902d5b1f23ead1a283e939ece0a2a1f
TurnerHelical <hunterde91@gmail.com>
2025-04-02 13:43:05 -0400

    started writing logic for the game

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function createPlayer(name) {
+    const getName = () => 
+}
+
+function dom(el1, el2) {
+    const findElement = () => document.querySelector(`${el1}`);
+    const createElement = () => document.createElement(`${el1}`);
+    const appendElement = () => el1.appendChild(`${el2}`);
+
+}
