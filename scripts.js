3efdb4a9d5601566ff889998be465762317c3c03
TurnerHelical <hunterde91@gmail.com>
2025-04-04 11:11:42 -0400

    added a start button to show on page load

--- scripts.js
+++ scripts.js
@@ -1,10 +1,22 @@
-function createPlayer(name) {
-    const getName = () => 
+// function createPlayer(name) {
+//     const getName = (name) =>  
+// }
+
+const player = player();
+const utils = dom();
+const game = gameBoard();
+
+function dom() {
+    const findElement = (el1) => document.querySelector(el1);
+    const createElement = (el1) => document.createElement(el1);
+    const appendElement = (el1, el2) => el1.appendChild(el2);
+    const changeAttribute = function(el1, attr, attrValue) {
+        const element = findElement(el1);
+        element.setAttribute(attr, attrValue);
+    }
+    return {findElement, createElement, appendElement, changeAttribute};
 }
 
-function dom(el1, el2) {
-    const findElement = () => document.querySelector(`${el1}`);
-    const createElement = () => document.createElement(`${el1}`);
-    const appendElement = () => el1.appendChild(`${el2}`);
-
+function gameBoard() {
+    
 }
